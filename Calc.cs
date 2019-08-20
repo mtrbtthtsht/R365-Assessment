9f1b6db206cd46f94e5729b25d0e12eae733f7ec
ConnorZ <47134172+mtrbtthtsht@users.noreply.github.com>
2019-08-19 17:54:06 -0700

    Step 6 support single custom delimiter

--- Calc.cs
+++ Calc.cs
@@ -6,47 +6,123 @@ namespace StringCalculator
 {
     public class Calc
     {
-        public static int Add(string numbers)
+        public int Add(string numbers)
         {
-            if (numbers == "") return 0;
+            if (string.IsNullOrEmpty(numbers)) return 0;
 
-            var delimiter = ",";
-            numbers = numbers.Replace("\n", delimiter);
+            var delimiter = GrabDelimiters(numbers);
+            var values = StringToIntegers(numbers, delimiter);
+            return SumOfIntegers(values);
+            
 
+
+        }
+        private int SumOfIntegers(IEnumerable<int> numbers)
+        {
+            var filteredNumbers = numbers.Where(x => x <= 1000);
+            return filteredNumbers.Sum();
+        }
+
+        private IEnumerable<int> StringToIntegers(string numbers, char[] delimiter)
+        {
+            var inputs = ChangeStringValues(numbers);
+            var symbols = GetNumbers(inputs, delimiter);
+            var digits = symbols.Select(int.Parse);
+            NegativeNumbersThrowException(digits);
+            return digits;
+        }
+
+        private void NegativeNumbersThrowException(IEnumerable<int> digits)
+        {
+            var negatives = digits.Where(x => x < 0);
+            if (negatives.Any())
+            {
+                var stringOfNegatives = String.Join(",", negatives);
+                throw new Exception($"negatives not allowed: {stringOfNegatives}");
+            }
+        }
+
+        private string[] GetNumbers(string numbers, char[] delimiter)
+        {
+            var symbols = numbers.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+            return symbols;
+        }
+
+        private string ChangeStringValues(string numbers)
+        {
+            var results = numbers;
             if (numbers.StartsWith("//"))
             {
-                delimiter = numbers[2].ToString();
-                numbers = numbers.Substring(4);
+                var indexOfNewLinedelimiter = numbers.IndexOf('\n');
+                results = numbers.Substring(indexOfNewLinedelimiter + 1);
             }
+            return results;
+        }
 
-            numbers = numbers.Replace("\n", delimiter);
-
-            var values = numbers.Split(',');
-            List<int> negatives = new List<int>();
-            var sum = 0;
-
-            foreach (var v in values)
+        private char[] GrabDelimiters(string numbers)
+        {
+            var delimiter = new[] { ',', '\n' };
+            if (numbers.StartsWith("//"))
             {
-                int value = int.Parse(v);
-                if (value > 1000) value = 0;
-
-                if (value < 0)
-                {
-                    negatives.Add(value);
-                }
-                sum += value;
+                delimiter = AddDelimiters(numbers, delimiter);
             }
+            return delimiter;
+        }
 
-            if (negatives.Count > 0)
-            {
-                string msg = "negatives not allowed: ";
-                for (int i = 0; i < negatives.Count; i++)
-                {
-                    msg += negatives[i] + ",";
-                }
-                throw new Exception(msg);
-            }
-            return sum;
+        private char[] AddDelimiters(string numbers, char[] delimiter)
+        {
+            var beginning = numbers.IndexOf("//");
+            var end = numbers.IndexOf("\n");
+            var symbols = numbers.Substring(beginning, (end - beginning)).ToCharArray();
+            return JoinDelimiters(delimiter, symbols);
+        }
+
+        private char[] JoinDelimiters(char[] delimiter, char[] symbols)
+        {
+            var delimiterLengthOne = delimiter.Length;
+            var newLength = delimiterLengthOne + symbols.Length;
+            Array.Resize(ref delimiter, newLength);
+            Array.Copy(symbols, 0, delimiter, delimiterLengthOne, symbols.Length);
+            return delimiter;
         }
     }
 }
+
+
+
+//numbers = numbers.Replace("\n", delimiter);
+
+//if (numbers.StartsWith("//"))
+//{
+//    delimiter = numbers[2].ToString();
+//    numbers = numbers.Substring(4);
+//}
+
+//numbers = numbers.Replace("\n", delimiter);
+
+//var values = numbers.Split(',');
+//List<int> negatives = new List<int>();
+//var sum = 0;
+
+//foreach (var v in values)
+//{
+//    int value = int.Parse(v);
+//    if (value > 1000) value = 0;
+
+//    if (value < 0)
+//    {
+//        negatives.Add(value);
+//    }
+//    sum += value;
+//}
+
+//if (negatives.Count > 0)
+//{
+//    string msg = "negatives not allowed: ";
+//    for (int i = 0; i < negatives.Count; i++)
+//    {
+//        msg += negatives[i] + ",";
+//    }
+//    throw new Exception(msg);
+//}
+//return sum;
--- StringCalculatorTests.cs
+++ StringCalculatorTests.cs
@@ -13,43 +13,73 @@ namespace StringCalculator
         [DataRow("",0)]
         public void Add_EmptyString(string input, int expected)
         {
-            Assert.AreEqual(expected, Calc.Add(input));
+            var calculator = new Calc();
+
+            Assert.AreEqual(expected, calculator.Add(input));
+        }
+        [DataTestMethod]
+        [DataRow("1", 1)]
+        public void Add_WhenSingleNumber_ReturnNumber(string input, int expected)
+        {
+            var calculator = new Calc();
+
+            Assert.AreEqual(expected, calculator.Add(input));
         }
         [DataTestMethod]
         [DataRow("1,20", 21)]
         public void Add_CommaDelimFormat(string input, int expected)
         {
+            var calculator = new Calc();
 
-            Assert.AreEqual(expected, Calc.Add(input));
+            Assert.AreEqual(expected, calculator.Add(input));
         }
         //Step 2. support more than two numbers
         [DataTestMethod]
         [DataRow("1,2,3", 6)]
         public void Add_MoreThanTwoNumbers(string input, int expected)
         {
-            Assert.AreEqual(expected, Calc.Add(input));
+            var calculator = new Calc();
+
+            Assert.AreEqual(expected, calculator.Add(input));
         }
         //Step 3. support newline char as delim
         [DataTestMethod]
         [DataRow("1\n2,3", 6)]
         public void Add_NewLineDelimiter(string input, int expected)
         {
-            Assert.AreEqual(expected, Calc.Add(input));
+            var calculator = new Calc();
+
+            Assert.AreEqual(expected, calculator.Add(input));
         }
         //Step 4. negative not allowed throw exception
         [DataTestMethod]
-        [DataRow("1,2,-3", "negatives not allowed: -3,")]
-        [DataRow("1,2,3,-4,-5,6,-7", "negatives not allowed: -4,-5,-7,")]
+        [DataRow("1,2,-3", "negatives not allowed: -3")]
+        [DataRow("1,2,3,-4,-5,6,-7", "negatives not allowed: -4,-5,-7")]
         public void Add_Negatives_ThrowsException(string input, string expected)
         {
-            Assert.AreEqual(expected, Assert.ThrowsException<Exception>(() => Calc.Add(input)).Message);
+            var calculator = new Calc();
+            
+            Assert.AreEqual(expected, Assert.ThrowsException<Exception>(() => calculator.Add(input)).Message);
         }
         //step 5 ignore numbers higher than 1000
         [DataTestMethod]
         [DataRow("3,5,1001", 8)]
         public void Add_WhenNumbersOver1000_DontAdd(string input, int expected)
         {
-            Assert.AreEqual(expected, Calc.Add(input));
+            var calculator = new Calc();
+
+            Assert.AreEqual(expected, calculator.Add(input));
         }
+        //Step 6 suppor single custom delimiter
+        [DataTestMethod]
+        [DataRow("//;\n2;5", 7)]
+        [DataRow("//^\n8^7\n5,2", 22)]
+        public void Add_SingleCustomDelimiter(string input, int expected)
+        {
+           var calculator = new Calc();
+
+            Assert.AreEqual(expected, calculator.Add(input));
+        }
+        
     }
 }
